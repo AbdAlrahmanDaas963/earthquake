e84cff8eb7759384224caae49dce3d811c6228d0
Abdalrahman <abdalrahmandaas963@gmail.com>
2023-03-24 00:58:24 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Login from "./screens/login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
